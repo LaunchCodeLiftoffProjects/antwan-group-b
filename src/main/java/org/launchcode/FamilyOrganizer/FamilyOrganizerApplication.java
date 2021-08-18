ac9721bfdef9429b29d6726cb862968328dccfee
Chris Rebuck <crebuck.apt@gmail.com>
2021-08-18 06:38:47 -0500

    Initiated FamilyOrganizerApplication. Initial Commit.

---
+++ src/main/java/org/launchcode/FamilyOrganizer/FamilyOrganizerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.FamilyOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/FamilyOrganizer/FamilyOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.FamilyOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
