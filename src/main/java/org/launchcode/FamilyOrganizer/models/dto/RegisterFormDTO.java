be31b35da6d963f92b146ecb9f124807194780e2
Chris Rebuck <crebuck.apt@gmail.com>
2021-08-23 19:46:59 -0500

    added family name to registration and it successfully is entered into table in database.

--- src/main/java/org/launchcode/FamilyOrganizer/models/dto/RegisterFormDTO.java
+++ src/main/java/org/launchcode/FamilyOrganizer/models/dto/RegisterFormDTO.java
@@ -1,8 +1,17 @@
 package org.launchcode.FamilyOrganizer.models.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class RegisterFormDTO extends LoginFormDTO {
 
     private String verifyPassword;
+
+    @NotNull
+    @NotBlank
+    @Size(message="Must be at least 3 characters long")
+    private String familyName;
 
     public String getVerifyPassword() {
         return verifyPassword;
@@ -12,4 +21,11 @@ public class RegisterFormDTO extends LoginFormDTO {
         this.verifyPassword = verifyPassword;
     }
 
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
 }
