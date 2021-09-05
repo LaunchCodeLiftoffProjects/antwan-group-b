503fc1373c35d42dd361678097f8bbec50e8b2f5
unknown <kdallen@accessus.net>
2021-09-05 11:57:26 -0500

    Update Post to Save Menu Items

--- src/main/java/org/launchcode/FamilyOrganizer/controllers/MenuController.java
+++ src/main/java/org/launchcode/FamilyOrganizer/controllers/MenuController.java
@@ -1,6 +1,7 @@
 package org.launchcode.FamilyOrganizer.controllers;
 
 import org.launchcode.FamilyOrganizer.data.MenuRepository;
+import org.launchcode.FamilyOrganizer.data.UserRepository;
 import org.launchcode.FamilyOrganizer.models.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,7 @@ public class MenuController {
 
     @Autowired
     private MenuRepository menuRepository;
+    private UserRepository userRepository;
 
     @GetMapping("add")
     public String menu(Model model) {
@@ -25,14 +27,14 @@ public class MenuController {
     }
 
     @PostMapping("add")
-    public String processMenuAddForm(@ModelAttribute Errors errors,
-                                     Model model) {
+    public String processMenuAddForm(@ModelAttribute Menu menuItem, Errors errors, Model model) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add");
             return "/menu/add";
         }
 
+        menuRepository.save(menuItem);
         return "/menu/add";
     }
 }
