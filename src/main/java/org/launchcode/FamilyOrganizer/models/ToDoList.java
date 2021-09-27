f096f3ee57f306cd0c11bf8ef29558dbc4e83ab8
ArpithaNagappa <arpithagn021@gmail.com>
2021-09-27 16:21:52 -0500

    Added Validations to To Do List Updated Fragments - Changed Add to Task to Add to List On Events Create Page - Fixed typo(Instead of End Time it was showing as Start Time)

--- src/main/java/org/launchcode/FamilyOrganizer/models/ToDoList.java
+++ src/main/java/org/launchcode/FamilyOrganizer/models/ToDoList.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -16,31 +15,59 @@ public class ToDoList extends AbstractEntity{
     @ManyToOne
     private User user;
 
+    @NotBlank(message= "To Do details is required.")
+    @Size(min=2, max=25, message="Must be between 2 and 25 characters long.")
     private String name;
 
+    @NotBlank(message= "Family member is required.")
+    @Size(min=2, max=25, message="Must be between 2 and 25 characters long.")
     private String member;
 
+
+    @Future(message = "Start Date should be greater than today's date")
+    @NotNull(message= "Start Date is required")
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern= "yyyy-MM-dd")
     @Column(name="startdate")
     private Date start;
 
-
+    @NotNull(message= "Start Time is required")
     @Temporal(TemporalType.TIME)
     @DateTimeFormat(pattern= "HH:mm")
     @Column(name="starttime")
     private Date starttime;
 
+    @Future(message = "End Date should be greater than today's date")
+    @NotNull(message= "End Date is required")
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern= "yyyy-MM-dd")
     @Column(name="enddate")
     private Date enddate;
 
-
+    @NotNull(message= "End Time is required")
     @Temporal(TemporalType.TIME)
     @DateTimeFormat(pattern= "HH:mm")
     @Column(name="endtime")
     private Date endtime;
+
+    //@AssertTrue(message="Start datetime should be less than end datetime")
+    public boolean isValidRange() {
+
+        if(this.start.compareTo(this.enddate)==0) {
+            if(this.starttime.compareTo(this.endtime)>=0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        else if (this.start.compareTo(this.enddate)>0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
 
     public ToDoList(String name, String member, Date start, Date starttime, Date enddate, Date endtime,  User user) {
         this.name = name;
